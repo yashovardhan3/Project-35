6e47ccbf030ab28482a77e3b4ebe991c069b9770
yashovardhan3 <76634258+yashovardhan3@users.noreply.github.com>
2021-04-16 18:02:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//Create variables here
+var foodStock! = 0;
+var feed = 0;
+var lastFed = 0;
+var foodStock = 0;
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+  if(x == 3){
+
+  }
+}
+(x);
+
+
